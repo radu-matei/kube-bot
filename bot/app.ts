355557699034820efa6e01172344ab95b81a151d
radu-matei <matei.radu94@gmail.com>
2017-09-26 11:45:44 +0300

    Update app
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

--- bot/app.ts
+++ bot/app.ts
@@ -46,7 +46,7 @@ bot.dialog('GetClusterInfo', function (session) {
         path: '/get/cluster'
     };
 
-    var body = 'I hope your demo fails! '
+    var body = 'Your demo will work perfectly! '
     http.get(options, response => {
         response.on('data', data => {
             body += data
